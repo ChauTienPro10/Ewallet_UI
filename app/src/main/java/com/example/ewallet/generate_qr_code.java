5b319b5718f39ecfba8b5883caf5d89c3b8f223a
Tat Thang <teddyle2003@gmail.com>
2024-04-13 00:14:57 +0700

    Gen QR code

---
+++ app/src/main/java/com/example/ewallet/generate_qr_code.java
@@ -0,0 +1,14 @@
+package com.example.ewallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class generate_qr_code extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_qr_code);
+    }
+}
