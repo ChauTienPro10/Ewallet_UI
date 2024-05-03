9555059254bd80a8029f44b2d813db967aea3aaa
tuantran47 <21110810@student.hcmute.edu.vn>
2024-05-03 21:19:16 +0700

    dialog password

--- app/src/main/java/com/example/ewallet/EditPhone.java
+++ app/src/main/java/com/example/ewallet/EditPhone.java
@@ -1,20 +1,34 @@
 package com.example.ewallet;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class EditPhone extends Fragment {
-
+    private LinearLayout linearLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_phone, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_password, container, false);
+
+        return view;
     }
+
+
+
 }
