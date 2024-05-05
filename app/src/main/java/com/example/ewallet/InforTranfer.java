6bbbfebcc74271505c7473acca0c815cb17efb4c
tuantran47 <21110810@student.hcmute.edu.vn>
2024-05-05 15:00:19 +0700

    infor_tranfer

---
+++ app/src/main/java/com/example/ewallet/InforTranfer.java
@@ -0,0 +1,14 @@
+package com.example.ewallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InforTranfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infor_tranfer);
+    }
+}
